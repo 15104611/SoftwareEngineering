1460af82ff1fa0bdbbcfcea9e5fb00d70c890f50
Joey <j.maloney6@nuigalway.ie>
2019-12-21 13:03:48 +0000

    Final Commit with Intended Failure

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -47,7 +47,7 @@ class StudentTest {
 
         student.setUsername("Moey Jaloney29");
         String newUserName = student.getUsername();
-        Assert.assertEquals("MoeyJaloney29", newUserName);
+        Assert.assertEquals("TestShouldFailHere", newUserName);
     }
 
     @Test
